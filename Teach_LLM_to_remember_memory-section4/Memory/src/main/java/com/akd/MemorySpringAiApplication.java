4d9daac1f5477897a8fab7cfa8f0bc38787c6067
AbirDas <das.abirkumar@gmail.com>
2026-09-21 21:19:36 +0530

    using H2 to store chat memory upto 10 last chat

---
+++ Teach_LLM_to_remember_memory-section4/Memory/src/main/java/com/akd/MemorySpringAiApplication.java
@@ -0,0 +1,13 @@
+package com.akd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemorySpringAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemorySpringAiApplication.class, args);
+	}
+
+}
--- Teach_LLM_to_remember_memory-section4/Memory/src/main/java/com/akd/config/ChatMemoryChatClientConfig.java
+++ Teach_LLM_to_remember_memory-section4/Memory/src/main/java/com/akd/config/ChatMemoryChatClientConfig.java
@@ -1,9 +1,18 @@
 package com.akd.config;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
 import org.springframework.context.annotation.Bean;
 
 public class ChatMemoryChatClientConfig {
+
+    @Bean
+    ChatMemory chatMemory(JdbcChatMemoryRepository jdbcChatMemoryRepository) {
+        return MessageWindowChatMemory.builder().maxMessages(10)
+                .chatMemoryRepository(jdbcChatMemoryRepository).build();
+    }
 
     @Bean("chatMemoryChatClient")
     public ChatClient chatClient(ChatClient.Builder chatClientBuilder) {
