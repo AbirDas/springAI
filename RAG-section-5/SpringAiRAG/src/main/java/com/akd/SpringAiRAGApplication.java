1a6b9c5db1aa68b163f2190432d402afe07fab38
AbirDas <das.abirkumar@gmail.com>
2026-09-21 22:14:39 +0530

    initial for Retrieval Augumented Generation setting up vector DB qdrant

---
+++ RAG-section-5/SpringAiRAG/src/main/java/com/akd/SpringAiRAGApplication.java
@@ -0,0 +1,13 @@
+package com.akd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiRAGApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiRAGApplication.class, args);
+	}
+
+}
