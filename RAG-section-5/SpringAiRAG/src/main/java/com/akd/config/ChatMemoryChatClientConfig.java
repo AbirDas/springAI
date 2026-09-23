f8613bc84bb02b20e3382869f4ed50e47875dc41
AbirDas <das.abirkumar@gmail.com>
2026-09-23 21:45:11 +0530

    RAG simple example with vectorStore

---
+++ RAG-section-5/SpringAiRAG/src/main/java/com/akd/config/ChatMemoryChatClientConfig.java
@@ -0,0 +1,22 @@
+package com.akd.config;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
+import org.springframework.context.annotation.Bean;
+
+public class ChatMemoryChatClientConfig {
+
+    @Bean
+    public ChatMemory chatMemory(JdbcChatMemoryRepository jdbcChatMemoryRepository) {
+        return MessageWindowChatMemory.builder().maxMessages(10)
+                .chatMemoryRepository(jdbcChatMemoryRepository).build();
+    }
+
+    @Bean("chatMemoryChatClient")
+    public ChatClient chatClient(ChatClient.Builder chatClientBuilder) {
+        return chatClientBuilder.build();
+    }
+
+}
