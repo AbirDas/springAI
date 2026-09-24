9473a87e8393d6668e9569641d692bbbed2d626e
AbirDas <das.abirkumar@gmail.com>
2026-09-24 12:21:00 +0530

    Example of document RAG with Tika-documnet-reader and chunking wile storing into vector db

--- RAG-section-5/SpringAiRAG/src/main/java/com/akd/config/ChatMemoryChatClientConfig.java
+++ RAG-section-5/SpringAiRAG/src/main/java/com/akd/config/ChatMemoryChatClientConfig.java
@@ -1,10 +1,17 @@
 package com.akd.config;
 
+import com.akd.advisor.TokenUsageAuditAdvisor;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.client.advisor.api.Advisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
+import org.springframework.ai.rag.advisor.RetrievalAugmentationAdvisor;
 import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 public class ChatMemoryChatClientConfig {
 
@@ -15,8 +22,13 @@ public class ChatMemoryChatClientConfig {
     }
 
     @Bean("chatMemoryChatClient")
-    public ChatClient chatClient(ChatClient.Builder chatClientBuilder) {
-        return chatClientBuilder.build();
+    public ChatClient chatClient(ChatClient.Builder chatClientBuilder, ChatMemory chatMemory) {
+        Advisor loggerAdvisor = new SimpleLoggerAdvisor();
+        Advisor tokenUsageAuditAdvisor = new TokenUsageAuditAdvisor();
+        Advisor memoryAdvisor = MessageChatMemoryAdvisor.builder(chatMemory).build();
+        return chatClientBuilder
+                .defaultAdvisors(List.of(loggerAdvisor, memoryAdvisor,tokenUsageAuditAdvisor))
+                .build();
     }
 
 }
